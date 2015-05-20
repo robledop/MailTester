ffa37ccfcb32765d9b5200c3ee7fd99605235b00
robledop <robledo@gmail.com>
2015-05-20 04:18:52 -0300

    .

--- MailTester/MailTester/MainWindow.xaml.cs
+++ MailTester/MailTester/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ARSoft.Tools.Net.Dns;
 
 namespace MailTester
 {
@@ -23,6 +25,57 @@ namespace MailTester
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Button1_Click(object sender, RoutedEventArgs e)
+		{
+			if ((string)Button1.Content == "Test")
+			{
+				string response = "";
+				var mxRecord = MxComboBox.SelectedItem as MxRecord;
+				var helper = new SocketHelper(mxRecord.ExchangeDomainName, 25);
+
+				var senderDomain = FromTextBox.Text.Split('@').Last();
+
+				helper.SendCommand("EHLO " + senderDomain);
+				StatusTextBlock.Text += "EHLO " + senderDomain + "\r\n";
+				response = helper.GetFullResponse();
+				StatusTextBlock.Text += response;
+
+				helper.SendCommand("MAIL FROM:<" + FromTextBox.Text + ">");
+				StatusTextBlock.Text += "MAIL FROM:<" + FromTextBox.Text + ">\r\n";
+				response = helper.GetFullResponse();
+				StatusTextBlock.Text += response;
+
+				helper.SendCommand("RCPT TO:<" + ToTextBox.Text + ">");
+				StatusTextBlock.Text += "RCPT TO:<" + ToTextBox.Text + ">\r\n";
+				StatusTextBlock.Text += helper.GetFullResponse();
+
+				helper.SendCommand("Data");
+				StatusTextBlock.Text += helper.GetFullResponse();
+
+				helper.SendCommand("\r\n");
+				helper.SendCommand(".");
+				helper.SendCommand("\r\n");
+				StatusTextBlock.Text += helper.GetFullResponse();
+
+				helper.SendCommand("QUIT");
+			}
+			else
+			{
+				var domainName = ToTextBox.Text.Split('@').Last();
+				var response = DnsClient.Default.Resolve(domainName, RecordType.Mx);
+				var records = response.AnswerRecords.OfType<MxRecord>();
+
+				foreach (var mxRecord in records)
+				{
+					MxComboBox.Items.Add(mxRecord);
+				}
+
+				MxComboBox.SelectedIndex = 0;
+				Button1.Content = "Test";
+			}
+
 		}
 	}
 }
---
+++ MailTester/MailTester/SocketHelper.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace MailTester
+{
+	public class SocketHelper
+	{
+		private readonly TcpClient _client;
+		private NetworkStream stream = null;
+		private StreamReader reader = null;
+		private StreamWriter writer = null;
+		private string resp = "";
+		private int state = -1;
+
+		public SocketHelper(string name, int port)
+		{
+			_client = new TcpClient(name, port);
+			stream = _client.GetStream();
+			reader = new StreamReader(stream);
+			writer = new StreamWriter(stream);
+		}
+
+		public SocketHelper(TcpClient tc)
+		{
+			_client = tc;
+			stream = _client.GetStream();
+			reader = new StreamReader(stream);
+			writer = new StreamWriter(stream);
+		}
+
+		public void SendData(byte[] bts)
+		{
+			if (GetResponseState() != 221)
+			{
+				stream.Write(bts, 0, bts.Length);
+				stream.Flush();
+			}
+		}
+
+		public void SendCommand(string cmd)
+		{
+			if (GetResponseState() != 221)
+			{
+				writer.WriteLine(cmd);
+				writer.Flush();
+			}
+		}
+
+		public string RecvResponse()
+		{
+			if (GetResponseState() != 221)
+				resp = reader.ReadLine();
+			else
+				resp = "221 closed!";
+
+			return resp;
+		}
+
+		public int GetResponseState()
+		{
+			if (resp.Length >= 3 && IsNumber(resp[0])
+			    && IsNumber(resp[1]) && IsNumber(resp[2]))
+				state = Convert.ToInt32(resp.Substring(0, 3));
+
+			return state;
+		}
+
+		private bool IsNumber(char c)
+		{
+			return c >= '0' && c <= '9';
+		}
+
+		public string GetFullResponse()
+		{
+			System.Text.StringBuilder sb = new System.Text.StringBuilder();
+			sb.Append(RecvResponse());
+			sb.Append("\r\n");
+			while (HaveNextResponse())
+			{
+				sb.Append(RecvResponse());
+				sb.Append("\r\n");
+			}
+			return sb.ToString();
+		}
+
+		public bool HaveNextResponse()
+		{
+			if (GetResponseState() > -1)
+			{
+				if (resp.Length >= 4 && resp[3] != ' ')
+					return true;
+				else
+					return false;
+			}
+			else
+				return false;
+		}
+	}
+}
